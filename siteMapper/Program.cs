1b3c9abbfdc3d6d0f461113f0f7b7a12cea06f96
Simon <simon.h.rosner@gmail.com>
2018-02-19 21:58:12 -0500

    Added object methods
    added important methods to objects.

--- siteMapper/Program.cs
+++ siteMapper/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace siteMapper
 {
     //objects
-    class Page
+    class Page : IEquatable<Page>
     {
+        public Website Parent { get; set; }
         public string URL { get; set; }
-        public IList<Page> Links { get; set; }
+        public List<Page> Links { get; set; }
         public Page(string url)
         {
             this.URL = url;
@@ -23,16 +25,68 @@ namespace siteMapper
         {
             this.Links.Add(new Page(link));
         }
+        public void AddAllLinks()
+        {
+            string pageContent = new System.Net.WebClient().DownloadString(this.URL);
+            //https://stackoverflow.com/questions/4750015/regular-expression-to-find-urls-within-a-string
+            Regex regx = new Regex("http://([\\w+?\\.\\w+])+([a-zA-Z0-9\\~\\!\\@\\#\\$\\%\\^\\&amp;\\*\\(\\)_\\-\\=\\+\\\\\\/\\?\\.\\:\\;\\'\\,]*)?", RegexOptions.IgnoreCase);
+            MatchCollection matches = regx.Matches(pageContent);
+            foreach (Match match in matches)
+            {
+                if (this.Links.Contains(new Page(match.ToString()))) //Links already contain a page with this url
+                {
+                    Page existingPage = Parent.Pages.Find(x => x.URL == match.ToString());
+                    AddLink(existingPage);
+                }
+                else
+                {
+                    AddLink(match.ToString());
+                }
+            }
+        }
+        public bool Equals(Page other)
+        {
+            if (this.URL == other.URL)
+            {
+                return true;
+            }
+            else { return false; }
+        }
     }
 
-    class Website
+    class Website : IEquatable<Website>
     {
         public string Domain { get; }
-        public IList<Page> Pages { get; set; }
+        public List<Page> Pages { get; set; }
+        public Website(string domain)
+        {
+            this.Domain = domain;
+            AddPage(domain);
+        }
+        public void AddPage(Page link)
+        {
+            this.Pages.Add(link);
+        }
+        public void AddPage(string link)
+        {
+            this.Pages.Add(new Page(link));
+        }
+        public void AddAllPages()
+        {
+            foreach(Page p in Pages)
+            {
 
+            }
+        }
+        public bool Equals(Website other)
+        {
+            if (this.Domain == other.Domain)
+            {
+                return true;
+            }
+            else { return false; }
+        }
     }
-
-    //methods
 
 
     static class Program
@@ -43,6 +97,22 @@ namespace siteMapper
         [STAThread]
         static void Main()
         {
+            //start console stuff
+            Console.WriteLine("Please enter url in the following format");
+            Console.WriteLine("http://website.com/Main");
+            string startPage = Console.ReadLine();
+            Website site = new Website(startPage);
+
+
+
+
+
+
+            //end console stuff
+
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
