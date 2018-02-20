df4d2ba2630decf2adb648afd0802555443ccc46
Simon <simon.h.rosner@gmail.com>
2018-02-19 23:11:59 -0500

    Test files correctly added

---
+++ siteMapperTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("siteMapperTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("siteMapperTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("394e5d70-7b53-4c40-a9ec-a1564137b5ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ siteMapperTests/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using siteMapper;
+
+namespace siteMapperTests
+{
+    [TestClass]
+    public class WebsiteTest
+    {
+        [TestMethod]
+        public void CreatWebsite()
+        {
+        }
+        [TestMethod]
+        public void AddPage1()
+        {
+        }
+        [TestMethod]
+        public void AddPage2()
+        {
+        }
+        [TestMethod]
+        public void AddAllPages()
+        {
+        }
+        [TestMethod]
+        public void Equals()
+        {
+        }
+    }
+    [TestClass]
+    public class PageTest
+    {
+        siteMapper.Website stubSite;
+        string testDomain;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            testDomain = "https://www.test.com/";
+            stubSite = new siteMapper.Website(testDomain);
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            stubSite = null;
+            testDomain = String.Empty;
+        }
+        [TestMethod]
+        public void CreatPage()
+        {
+            Page testPage = new Page(testDomain, stubSite);
+
+            Assert.AreEqual(testPage.URL, testDomain);
+        }
+        [TestMethod]
+        public void AddLink1()
+        {
+            Page testPage = new Page(testDomain, stubSite);
+            Page testPage2 = new Page(testDomain + "/contact", stubSite);
+            testPage.AddLink(testPage);
+
+            Assert.IsTrue(testPage.Links.Contains(testPage2));
+        }
+        [TestMethod]
+        public void AddLink2()
+        {
+            Page testPage = new Page(testDomain, stubSite);
+            Page testPage2 = new Page(testDomain + "/contact", stubSite);
+            testPage.AddLink(testDomain + "/contact");
+
+            Assert.IsTrue(testPage.Links.Contains(testPage2));
+        }
+        [TestMethod]
+        public void AddAllLinks()
+        {
+            Page testPage = new Page(testDomain, stubSite);
+            testPage.AddAllLinks();
+
+            Assert.IsNotNull(testPage.Links);
+            Assert.IsFalse(testPage.Links.Count <= 0);
+        }
+        [TestMethod]
+        public void Equals()
+        {
+            Page testPage = new Page(testDomain, stubSite);
+            Page testPage2 = new Page(testDomain, stubSite);
+            Page testPage3 = new Page(testDomain + "/privacy-policy", stubSite);
+            Page testPage4 = new Page("www.twest.com", stubSite);
+
+            Assert.AreEqual(testPage, testPage2);
+            Assert.IsTrue(testPage.Equals(testPage2));
+            Assert.AreNotEqual(testPage, testPage3);
+            Assert.IsFalse(testPage.Equals(testPage3));
+            Assert.AreNotEqual(testPage, testPage4);
+            Assert.IsFalse(testPage.Equals(testPage4));
+        }
+    }
+}
